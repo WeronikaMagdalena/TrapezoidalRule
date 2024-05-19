f0a7be08a217641c281b95715a9952b2f7ff902f
WeronikaMagdalena <alu0101696997@ull.edu.es>
2024-05-19 17:11:49 +0000

    trapezoids lines

--- src/home-work/View/trapezoids-view.ts
+++ src/home-work/View/trapezoids-view.ts
@@ -12,28 +12,30 @@
 
 export class TrapezoidsView {
 
-  draw(canvas: HTMLCanvasElement, numberOfTrapezoids: number) {
+  draw(canvas: HTMLCanvasElement, numberOfTrapezoids: number, { x, y }: { x: number[], y: number[] }, minX: number, maxX: number, { minY, maxY }: { minY: number, maxY: number }) {
     const margin = 20;
     const width = canvas.width - 2 * margin;
     const height = canvas.height - 2 * margin;
+    const scaleX = width / (maxX - minX);
+    const scaleY = height / (maxY - minY);
     const context = canvas.getContext("2d")!;
     context.fillStyle = 'red';
     context.strokeStyle = 'darkred';
-    context.beginPath();
-    context.arc(margin, 100, 4, 0, 2 * Math.PI);
-    context.fill();
     context.closePath();
     for (let i = 0; i < numberOfTrapezoids; i++) {
       context.beginPath();
-      context.moveTo(margin + width / numberOfTrapezoids * i + 4, 100);
+      context.moveTo(margin + width / numberOfTrapezoids * i, height - (y[((maxX - minX) / 0.05) / numberOfTrapezoids * i] - minY) * scaleY + margin);
       context.lineWidth = 1;
-      context.lineTo(margin + width / numberOfTrapezoids * (i + 1), 100);
+      context.lineTo(margin + width / numberOfTrapezoids * (i + 1), height - (y[((maxX - minX) / 0.05) / numberOfTrapezoids * (i + 1)] - minY) * scaleY + margin);
       context.stroke();
       context.beginPath();
-      context.arc(margin + width / numberOfTrapezoids * (i + 1), 100, 4, 0, 2 * Math.PI);
+      context.arc(margin + width / numberOfTrapezoids * i, height - (y[((maxX - minX) / 0.05) / numberOfTrapezoids * i] - minY) * scaleY + margin, 4, 0, 2 * Math.PI);
       context.fill();
       context.closePath();
     }
+    context.beginPath();
+    context.arc(margin + width, height - (y[y.length - 1] - minY) * scaleY + margin, 4, 0, 2 * Math.PI);
+    context.fill();
   }
 
 }
